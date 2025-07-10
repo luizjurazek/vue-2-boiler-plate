49803ee520d22231d0301b1653fa6324a4ca3f8a
Luiz Jurazek <luizjurazek@gmail.com>
2025-07-10 17:21:07 -0300

    chore: config some libs

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended", // <- ativa o prettier no eslint
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error", // <- marca erro se fugir do prettier
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
